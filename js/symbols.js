a79b832b6d01c5d774d5ac20dad69c3aa95a2da4
faa0311 <yukinashi0311@gmail.com>
2021-02-15 01:50:20 +0900

    通貨追加

---
+++ js/symbols.js
@@ -0,0 +1,143 @@
+// 通貨換算テーブル
+var symbols = {
+    "$": {
+        "rate": 110.0,
+        "code": "USD"
+    },
+    "A$": {
+        "rate": 73.67,
+        "code": "AUD"
+    },
+    "CA$": {
+        "rate": 77,
+        "code": "CAD"
+    },
+    "CHF&nbsp;": {
+        "rate": 113.0,
+        "code": "CHF"
+    },
+    "COP&nbsp;": {
+        "rate": 0.03,
+        "code": "COP"
+    },
+    "HK$": {
+        "rate": 13.8,
+        "code": "HKD"
+    },
+    "HUF&nbsp;": {
+        "rate": 0.34,
+        "code": "HUF"
+    },
+    "MX$": {
+        "rate": 4.72,
+        "code": "MXN"
+    },
+    "NT$": {
+        "rate": 3,
+        "code": "TWD"
+    },
+    "NZ$": {
+        "rate": 68.86,
+        "code": "NZD"
+    },
+    "PHP&nbsp;": {
+        "rate": 2.14,
+        "code": "PHP"
+    },
+    "PLN&nbsp;": {
+        "rate": 27.01,
+        "code": "PLN"
+    },
+    "R$": {
+        "rate": 20.14,
+        "code": "BRL"
+    },
+    "RUB&nbsp;": {
+        "rate": 1.5,
+        "code": "RUB"
+    },
+    "SEK&nbsp;": {
+        "rate": 11.48,
+        "code": "SEK"
+    },
+    "£": {
+        "rate": 135.0,
+        "code": "GBP"
+    },
+    "₩": {
+        "rate": 0.1,
+        "code": "KRW"
+    },
+    "€": {
+        "rate": 120,
+        "code": "EUR"
+    },
+    "₹": {
+        "rate": 1.42,
+        "code": "INR"
+    },
+    "￥": {
+        "rate": 1.0,
+        "code": "JPY"
+    },
+    "PEN&nbsp;": {
+        "rate": 30.56,
+        "code": "PEN"
+    },
+    "ARS&nbsp;": {
+        "rate": 1.53,
+        "code": "ARS"
+    },
+    "CLP&nbsp;": {
+        "rate": 0.13,
+        "code": "CLP"
+    },
+    "NOK&nbsp;": {
+        "rate": 11.08,
+        "code": "NOK"
+    },
+    "BAM&nbsp;": {
+        "rate": 61.44,
+        "code": "BAM"
+    },
+    "SGD&nbsp;": {
+        "rate": 77.02,
+        "code": "SGD"
+    },
+    "CZK&nbsp;": {
+        "rate": 4.49,
+        "code": "CZK"
+    },
+    "ZAR&nbsp;": {
+        "rate": 6.05,
+        "code": "ZAR"
+    },
+    "RON&nbsp;": {
+        "rate": 25.91,
+        "code": "RON"
+    },
+    "BYN&nbsp;": {
+        "rate": 43.16,
+        "code": "BYN"
+    },
+    "UYU&nbsp;": {
+        "rate": 2.49,
+        "code": "UYU"
+    },
+    "DKK&nbsp;": {
+        "rate": 17.11,
+        "code": "DKK"
+    },
+    "CRC&nbsp;": {
+        "rate": 0.17,
+        "code": "CRC"
+    },
+    "RSD&nbsp;": {
+        "rate": 1.08,
+        "code": "RSD"
+    },
+    "DOP&nbsp;": {
+        "rate": 1.81,
+        "code": "DOP"
+    },
+};
